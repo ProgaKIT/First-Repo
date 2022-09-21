9ff9ae5026f6df67297c267aa276a2a17bcf6c38
MrGood03 <113553866+MrGood03@users.noreply.github.com>
2022-09-21 23:32:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print()
+print()
+print("**********            @@@@@    @@@@@                   ******")
+print(" *********           @     @  @     @                 *******")
+print("  ********              @@@   @     @                ********")
+print("   *******           @@       @     @               *********")
+print("    ******          @@@@@@@    @@@@@   th          ********")
+print("     *****       _____________________________    ********")
+print("      ****         C   E   N   T  U   R   Y      ******* ")
+print("       ***       _____________________________  *****")
+print("        **       @@@@@ @@@@@ @     @ @@@@@       ***")
+print("        ==         @   @      @   @    @         ==")
+print("      __!!__       @   @@@@@    @      @       __!!__")
+print("      !    !       @   @      @   @    @       !    !")
+print("  ____!____!____   @   @@@@@ @     @   @ ______!____!_______" )
