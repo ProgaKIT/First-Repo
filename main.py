96ac1c80decfccef010ec383e3e31d15d06f31db
moonlin <moonlin.coder@gmail.com>
2022-09-21 23:05:22 +0300

    Обнова 1 Добавлен важный функционал предоставляющий пользователю возможность лицезреть картиночки в консоли (точно не позаимствованные из короваскажи в линуксе)

---
+++ main.py
@@ -0,0 +1,35 @@
+print("Hello world!")
+print("World: Sorry i have a boyfriend.")
+
+print(
+    r"""
+ _____________________ 
+< Moonlin was here?\n >
+ --------------------- 
+          \
+           \
+            \          __---__
+                    _-       /--______
+               __--( /     \ )XXXXXXXXXXX\v.
+             .-XXX(   O   O  )XXXXXXXXXXXXXXX-
+            /XXX(       U     )        XXXXXXX\
+          /XXXXX(              )--_  XXXXXXXXXXX\
+         /XXXXX/ (      O     )   XXXXXX   \XXXXX\
+         XXXXX/   /            XXXXXX   \__ \XXXXX
+         XXXXXX__/          XXXXXX         \__---->
+ ---___  XXX__/          XXXXXX      \__         /
+   \-  --__/   ___/\  XXXXXX            /  ___--/=
+    \-\    ___/    XXXXXX              '--- XXXXXX
+       \-\/XXX\ XXXXXX                      /XXXXX
+         \XXXXXXXXX   \                    /XXXXX/
+          \XXXXXX      >                 _/XXXXX/
+            \XXXXX--__/              __-- XXXX/
+             -XXXXXXXX---------------  XXXXXX-
+                \XXXXXXXXXXXXXXXXXXXXXXXXXX/
+                  ""VXXXXXXXXXXXXXXXXXXV""
+
+    """
+)
+
+# я слижком ленив для ascii art-ов
+# cowsay -f ghostbusters
