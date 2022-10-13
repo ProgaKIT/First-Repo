9c979db73f994e5f7004898395077cbb904d39e7
VisualBasicEnjoyer <nikita.kanaev@bk.ru>
2022-10-14 00:23:31 +0300

    func1

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-import functools
-import math
 while True:
-    print(
-        "Выберите арифметическое действие. 1 - Сложение, 2 - Вычитание, 3 - Умножение, 4 - Деление, 5 - Квадратное уравнение")
+    print("Выберите действие. 1 - Минимум из трёх чисел, 2 - Количество цифр в числе, 3 - Сумма чисел от 1 до N, 4 - Факториал")
     action = int(input())
-    if action == 5:
-        entered_list = input("Введите a b c через пробел: ").split()
-        num_list = list(map(float, entered_list))
-        D = num_list[1] ** 2 - 4 * num_list[0] * num_list[2]
-        if D < 0:
-            print("Нет решений.")
-        if D == 0:
-            print("D=0")
-            x = float(-1 * num_list[1] / 2 * num_list[0])
-            print("x=" + str(x))
-        if D > 0:
-            print("D=" + str(D))
-            x1 = float((-1 * num_list[1] + math.sqrt(D)) / (2 * num_list[0]))
-            x2 = float((-1 * num_list[1] - math.sqrt(D)) / (2 * num_list[0]))
-            print("x1=" + str(x1))
-            print("x2=" + str(x2))
-    elif action:
-        entered_list = input("Введите список чисел, разделенных пробелом: ").split()
-        num_list = list(map(int, entered_list))
-        print("Список чисел: ", num_list)
-        if action == 1:
-            print("Сумма списка:", functools.reduce(lambda x, y: x + y, num_list))
-        if action == 2:
-            print("Разность списка:", functools.reduce(lambda x, y: x - y, num_list))
-        if action == 3:
-            print("Произведение списка:", functools.reduce(lambda x, y: x * y, num_list))
-        if action == 4:
-            print("Частное списка:", functools.reduce(lambda x, y: x / y, num_list))
+    if action == 1:
+        def minimum(a, b, c):
+            print("Минимальное число: ", (min(a, b, c)))
+        num1 = int(input("Введите первое число: "))
+        num2 = int(input("Введите второе число: "))
+        num3 = int(input("Введите третье число: "))
+        minimum(num1, num2, num3)
+    if action == 2:
+        def quantity(a):
+            count = 0
+            while a > 0:
+                a = a // 10;
+                count += 1
+            print("Количество цифр числа:", count)
+        number = int(input("Введите число для поиска количества цифр: "))
+        quantity(number)
+    if action == 3:
+        def quantity2(a):
+            amount = 0
+            for i in range(quantityN):
+                z = int(input("Введите число: "))
+                amount += z
+            print("Сумма чисел: ", amount)
+        quantityN = int(input("Введите N (сумма чисел от 1 до N): "))
+        quantity2(quantityN)
+    if action == 4:
+        def factorial(a):
+            factorial = 1
+            while a > 1:
+                factorial *= a
+                a -= 1
+            print("Факториал: ", factorial)
+        fac = int(input("Введите число для поиска факториала: "))
+        factorial(fac)
